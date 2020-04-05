717834fa6d056535134e9f567377365e3270cf55
aniketaxeraj <56559009+aniketaxeraj@users.noreply.github.com>
2020-04-05 13:07:39 +0100

    Update a.js

--- rough_work/node/Callback/a.js
+++ rough_work/node/Callback/a.js
@@ -1,14 +1,13 @@
 var array = [1,2,3,4,5,6,7]
 
 function check(item,callback) {
-    ((value) => {
-        for(i of value){
-            var index = value.indexOf(i)
-            callback(i,index,value)
+    
+        for(i of item){
+            var index = item.indexOf(i)
+            callback(i,index,item)
         }
-    })(item)
 }
 
 check(array,(x,y,z) => {
-console.log(z)
+console.log(y)
 })
